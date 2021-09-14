d019c5adcd2af20c7787a004cd8f4aca7b7e8088
Gio <giovanni.leonardo@gmail.com>
2021-09-14 11:18:15 +0000

    copying files and folders

--- mix-list/mix_list.py
+++ mix-list/mix_list.py
@@ -4,4 +4,4 @@ print("The first item in the list (IP): " + my_list[0] )
 print("The second item in the list (port): " + str(my_list[1]) )
 print("The last item in the list (state): " + my_list[2] )
 new_list = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
-print("The IP address are:",my_list[-1),new_list[-1]
+print("The IP address are:",my_list[0)],new_list[3]
